5e0db9c903d8c5f8629779d0eeee8da668e5373a
Gustavo <gustavo.carvalho.ismart@gmail.com>
2023-09-15 00:19:32 -0300

    feat: Search box and modal button

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
+import SearchBox from './components/SearchBox.vue'
 </script>
 
 <template>
   <div class="border-b border-green p-4 text-center">
+    <SearchBox />
   </div>
+  <button
+    class="fixed bottom-5 right-5 bg-green w-20 h-20 rounded-full grid place-content-center text-8xl text-white">+</button>
 </template>
